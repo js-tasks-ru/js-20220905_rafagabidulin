4795d61da77c7420f348e98ac2d552ac758fbd47
rafagabidulin <rafagabidulin@gmail.com>
2022-09-13 23:15:37 +0300

    task 1-create-getter completed

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
 
+  return function(obj) {
+    let result = obj;
+
+    for (const prop of props) {
+      if (result === undefined) {
+        return undefined;
+      }
+      result = result[prop];
+    }
+    return result;
+  };
 }
